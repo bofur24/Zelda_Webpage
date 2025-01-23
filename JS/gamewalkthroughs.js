1e5adb1a7e57d0eeb18dcbf20701efc167239368
Keith Heffington <lightingcount_@hotmail.com>
2025-01-23 11:22:57 -0600

    Setting up page GameWalkthroughs

---
+++ JS/gamewalkthroughs.js
@@ -0,0 +1,39 @@
+
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+
+// Tab toggle between games on game hub pages
+  function openGame(evt, gameName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("game");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
+    }
+    document.getElementById(gameName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+  }
